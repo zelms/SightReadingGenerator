ceaabc6dfc704f6839f11dc464cb9df9df1834eb
zelms <69173015+zelms@users.noreply.github.com>
2025-05-25 16:07:01 -0700

    Fixes from running tests

--- Notes.py
+++ Notes.py
@@ -4,7 +4,7 @@ sharpSeries = ['c', 'cs', 'd', 'ds', 'e', 'f', 'fs', 'g', 'gs', 'a', 'as', 'b']
 flatSeries = ['c', 'df', 'd', 'ef', 'e', 'f', 'gf', 'g', 'af', 'a', 'bf', 'b']
 mixedSeries = ['c', 'cs', 'd', 'ef', 'e', 'f', 'fs', 'g', 'af', 'a', 'bf', 'b']
 commonRhythms = ['1', '2', '4', '8', '16']
-trebleMiddleRange = (0, 3)
+trebleMiddleRange = (1, 3)
 
 def getNote(noteIdx, octave, notes, rhythmIdx, rhythm):
     if(octave > 0):
@@ -16,14 +16,14 @@ def getNote(noteIdx, octave, notes, rhythmIdx, rhythm):
 def getRandomNote(series = mixedSeries, range = trebleMiddleRange, rhythms = commonRhythms):
     if(len(series) == 0):
         print("Invalid note series! Make sure the series has at least one note")
-        return ""
+        return None
     
-    if(len(range) != 2):
-        print("Invalid range! Make sure range is a tuple containing two elements")
-        return ""
+    if(type(range) is not tuple or len(range) != 2 or range[0] == range[1]):
+        print("Invalid range! Make sure range is a tuple containing two unique elements")
+        return None
         
     if(len(rhythms) == 0):
         print("Invalid rhythm series! Make sure the series has at least one rhythm")
-        return ""    
+        return None    
         
-    return getNote(random.randrange(0, len(series)), random.randrange(range[0], range[1]), series, random.randrange(1, len(rhythms)), rhythms)
+    return getNote(random.randrange(0, len(series)), random.randrange(range[0], range[1]), series, random.randrange(0, len(rhythms)), rhythms)
